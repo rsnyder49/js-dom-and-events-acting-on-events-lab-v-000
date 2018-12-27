ff1cfb0be31a2162d10b4c3c514fd83421dccfca
Robert Snyder <r.snyder49@gmail.com>
2018-12-27 21:34:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,6 @@ function addNewElementAsLi() {
 }
 
 function addNewLiOnClick() {
-  let submit = document.querySelector('input[type= 'submit')
+  let submit = document.querySelector("input[type= 'submit'");
+  
 }
