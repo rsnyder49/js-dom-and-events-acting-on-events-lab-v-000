ffaa03f5d8eb2346051f882b373cea7e9b18741d
Robert Snyder <r.snyder49@gmail.com>
2018-12-27 21:38:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function addNewLiOnClick() {
 function clearEmployeeListOnLinkClick() {
   let link = document.querySelector('a');
   let ul = document.querySelector('ul');
-  link.addEventListener()
+  link.addEventListener('click', )
 }
 
 
