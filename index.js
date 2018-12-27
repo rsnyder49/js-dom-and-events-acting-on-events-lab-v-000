746980f80000f3e72035ac48e39ffbeca66e3e2c
Robert Snyder <r.snyder49@gmail.com>
2018-12-27 21:35:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function addNewElementAsLi() {
 function addNewLiOnClick() {
   let submit = document.querySelector("input[type= 'submit'");
   submit.addEventListener('click', function(e) {
-    
-  })
+    addNewElementAsLi();
+  });
 }
