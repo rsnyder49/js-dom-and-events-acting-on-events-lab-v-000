90dc3bb3213e1f2e7c1ff94b42b2242f68b882b2
Robert Snyder <r.snyder49@gmail.com>
2018-12-27 21:40:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+      event.preventDefault();
+    });
 }
 
-preventRefreshOnSubmit()
+preventRefreshOnSubmit();
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  let ele = document.createElement('li');
+  ele.innerHTML = retrieveEmployeeInformation();
+  let list = document.querySelector('.employee-list');
+  list.append(ele);
+}
+
+let input = document.querySelector('input');
+
+function addNewLiOnClick() {
+  let submit = document.querySelector("input[type= 'submit'");
+  submit.addEventListener('click', function(e) {
+    addNewElementAsLi();
+    input.value = '';
+  });
+}
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a');
+  let ul = document.querySelector('ul');
+  link.addEventListener('click', function(e) {
+    ul.innerHTML = '';
+  });
+}
+
+
