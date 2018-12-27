081a0cf97c26c0139083b188042826878793dede
Robert Snyder <r.snyder49@gmail.com>
2018-12-27 21:34:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,7 @@ function addNewElementAsLi() {
 
 function addNewLiOnClick() {
   let submit = document.querySelector("input[type= 'submit'");
-  submit.addEventListener('click', function(e))
+  submit.addEventListener('click', function(e) {
+    
+  })
 }
